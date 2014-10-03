7a4565eee69bc0eed98e03ea2d24073a38e99a58
carter johnson <carter32lax@yahoo.com>
2014-10-03 13:44:50 -0700

    first commit

---
+++ public_html/js/snake.js
@@ -0,0 +1,19 @@
+var snake;
+
+var context;
+var screenWidth;
+var screenHeight;
+
+function gameInitalize() {
+    var canvas = document.getElementbyId("game-screen");
+    context = canvas.getContext("2d");
+    screenWidth = window.innerWidth;
+}
+
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    
+}
